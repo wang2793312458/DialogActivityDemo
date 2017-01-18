7e8ff73fd48e76243db09b88406573c631b8ea5c
wz2793312458 <wz2793312458@163.com>
2017-01-18 13:28:55 +0800

    Initial commit

---
+++ app/src/main/java/com/velue/myapplication/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.velue.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
---
+++ app/src/main/java/com/velue/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.velue.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.cc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
+    }
+}
